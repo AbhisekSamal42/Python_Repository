ea798f993e8962b79b6fa3b0217240b55fe03059
Abhisek Samal <samalabhisek42@gmail.com>
2024-08-19 09:03:38 +0530

    Add files via upload
    List comprehension method in python.

---
+++ List_Comprehension.py
@@ -0,0 +1,12 @@
+#Wap to print num in a  given range.
+
+print([num for num in range(10,21)])
+
+#Wap to print the string in to list.
+
+s='RAMESWARM'
+print([s[ind] for ind in range(len(s))])
+
+#Wap to print Evev number in a given range.
+
+print(['Enen Number' if num%2==0 else 'Odd Number' for num in range(50,60)])
