182fbfc75ea752115c153a4cc6ebe73ba5fe5a2c
Abhisek Samal <samalabhisek42@gmail.com>
2024-12-10 07:54:29 +0530

    Delete Q18.py

--- Q18.py
+++
@@ -1,9 +0,0 @@
-var=int(input("Enter a number: "))
-space=var-1
-for st in range(1,6):
-    for sp in range(space):
-        print(' ',end=' ')
-    for val in range(st,0,-1):
-        print(val,end=' ')
-    print()
-    space=space-1
